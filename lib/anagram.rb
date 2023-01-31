c6c088d626b0a4c585992858eadb7c51d1285111
Xavier Dunkley <xavierxv18@gmail.com>
2023-01-31 17:59:26 -0500

    Completed lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        
+
+        array.select {|n| @word.chars.sort == n.chars.sort}
+        # print array
+        # for element in array do
+        #    if @word.chars.sort == element.chars.sort
+        #         puts element
+        #    end
+        # end
+
+    end
+end
